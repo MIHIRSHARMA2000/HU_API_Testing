ea3153afb14a0906f3bb8f77ee5c53758f79f8e2
MIHIRSHARMA2000 <mihirsharma@deloitte.com>
2022-03-31 16:32:55 +0530

    mini_assignment_2

---
+++ mini_assignment_2/src/test/java/RestAssured_get.java
@@ -0,0 +1,30 @@
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RestAssured_get {
+    ResponseSpecification responseSpecification;
+    @BeforeTest
+    public void setup(){
+        ResponseSpecBuilder builder = new ResponseSpecBuilder();
+        builder.expectStatusCode(200);
+        builder.expectHeader("Content-Type","application/json; charset=utf-8");
+        builder.expectBody("userId[39]",equalTo(4));
+        builder.expectBody("title[39]",equalTo("enim quo cumque"));
+        responseSpecification = builder.build();
+    }
+    @Test
+    public void Response1(){
+        when()
+                .get("https://jsonplaceholder.typicode.com/posts").
+                then()
+                .spec(responseSpecification)
+                 .log().all();
+        //.body("userId[39]",equalTo(4))
+        //.body("title[39]",equalTo("enim quo cumque"));
+    }
+}
